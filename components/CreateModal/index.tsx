d50f87e91304f9f22e7c859a3e3f4991fc971959
quypq0420 <phanquangquy9a5@gmail.com>
2023-10-17 19:05:06 +0700

    Create Drag & Drop event

--- components/CreateModal/index.tsx
+++ components/CreateModal/index.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import {useRef} from "react";
+import { useRef, useState } from "react";
 
 export interface CreateModalProps {
     isShow: Function
@@ -7,8 +7,9 @@ export interface CreateModalProps {
 
 export default function CreateModal(props: CreateModalProps) {
     const { isShow } = props;
+    const [isDrag, setDrag] = useState(false);
     const modalElement = useRef<HTMLDivElement>(null);
-    
+
 
     /**
      * Handle when close modal by clicking into overlay.
@@ -20,33 +21,43 @@ export default function CreateModal(props: CreateModalProps) {
         }
     }
 
+    const handleDrop = (e: any) => {
+        e.preventDefault();
+        console.log('drop', e.dataTransfer.files);
+    }
+
+    const handleDragOver = (e: any) => {
+        e.preventDefault();
+        console.log('over', e.dataTransfer.files);
+    }
+
     return (
-        <div 
+        <div
             className="modal h-screen w-full fixed left-0 top-0 flex justify-center items-center bg-black bg-opacity-50"
             onClick={closeModal}
             ref={modalElement}
         >
             {/* <!-- modal --> */}
-            <div 
-                className="bg-white rounded shadow-lg w-10/12 md:w-1/3"
-                onClick={e => {
-                    console.log('321');
-                    e.stopPropagation();
-                }}
-            >
+            <div className="bg-white rounded shadow-lg w-10/12 md:w-1/3">
                 {/* <!-- modal header --> */}
                 <div className="border-b px-4 py-2 flex justify-center items-center">
                     <h3 className="font-semibold text-lg">Create new post</h3>
                 </div>
                 {/* <!-- modal body --> */}
-                <div className="p-3 mt-10 flex justify-center">
-                    <Image src="/images/image_icon.png" alt="" width={100} height={100}></Image>
-                </div>
-                <div className="pb-3 flex justify-center text-xl">
-                    <span>Drag photos or videos here</span>
-                </div>
-                <div className="flex justify-center items-center w-100 mb-20 p-3">
-                    <button className="bg-blue-600 hover:bg-blue-700 px-3 py-1 rounded text-white">Select from computer</button>
+                <div onDrop={handleDrop} onDragOver={handleDragOver} className={`border border-indigo-600 ${isDrag ? 'border-dashed' : ''} z-20`}>
+                    <div className="p-3 mt-10 flex justify-center">
+                        <Image src="/images/image_icon.png" alt="" width={100} height={100}></Image>
+                    </div>
+                    <div className="pb-3 flex justify-center text-xl">
+                        <span>Drag photos or videos here</span>
+                    </div>
+                    <div className="flex justify-center items-center w-100 mb-20 p-3">
+                        <label htmlFor="inputFile" className="bg-blue-600 hover:bg-blue-700 px-3 py-1 rounded text-white">Choose</label>
+                        <input
+                            id="inputFile"
+                            type="file"
+                        />
+                    </div>
                 </div>
             </div>
         </div>
