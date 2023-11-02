1c4223b745c37d8d2980900215e61163b28acd4f
quypq0420 <phanquangquy9a5@gmail.com>
2023-11-02 17:29:07 +0700

    Fix UI create post

--- components/CreateModal/index.tsx
+++ components/CreateModal/index.tsx
@@ -10,6 +10,7 @@ export interface CreateModalProps {
 export default function CreateModal(props: CreateModalProps) {
     const { isShow } = props;
     const [fileImage, setFileImage] = useState('');
+    const [urlImage, setUrlImage] = useState('');
     const modalElement = useRef<HTMLDivElement>(null);
 
 
@@ -25,13 +26,13 @@ export default function CreateModal(props: CreateModalProps) {
     }
 
     const handleDrop = async (e: any, type: string) => {
-        if(type === 'drop') {
+        if (type === 'drop') {
             e.preventDefault();
             setFileImage(e.dataTransfer.files);
             const fileRef = ref(uploadImage, 'images/post');
             uploadBytes(fileRef, e.dataTransfer.files[0]).then(data => {
                 getDownloadURL(data.ref).then(URL => {
-                    console.log(URL);
+                    setUrlImage(URL);
                 })
             })
         } else {
@@ -39,7 +40,7 @@ export default function CreateModal(props: CreateModalProps) {
             const fileRef = ref(uploadImage, 'images/post1');
             uploadBytes(fileRef, e.target.files[0]).then(data => {
                 getDownloadURL(data.ref).then(URL => {
-                    console.log(URL);
+                    setUrlImage(URL);
                 })
             })
         }
@@ -56,8 +57,8 @@ export default function CreateModal(props: CreateModalProps) {
             ref={modalElement}
         >
             {/* <!-- modal --> */}
-            <div 
-                className="bg-white rounded shadow-lg w-10/12 md:w-1/3"
+            <div
+                className="bg-white rounded shadow-lg"
                 onClick={e => { e.stopPropagation() }}
             >
                 {/* <!-- modal header --> */}
@@ -65,26 +66,36 @@ export default function CreateModal(props: CreateModalProps) {
                     <h3 className="font-semibold text-lg">Create new post</h3>
                 </div>
                 {/* <!-- modal body --> */}
-                <div 
-                    onDrop={(e) => handleDrop(e, 'drop')} 
+                <div
+                    onDrop={(e) => handleDrop(e, 'drop')}
                     onDragOver={handleDragOver}
-                    className='py-24'
+                    className='flex justify-center flex-col w-[635px] h-[635px] relative'
                 >
-                    <div className="p-3 mt-10 flex justify-center">
-                        <Image src="/images/image_icon.png" alt="" width={100} height={100}></Image>
-                    </div>
-                    <div className="pb-3 flex justify-center text-xl">
-                        <span>Drag photos or videos here</span>
-                    </div>
-                    <div className="flex justify-center items-center w-100 mb-20 p-3">
-                        <label htmlFor="inputFile" className="bg-blue-600 hover:bg-blue-700 px-3 py-1 rounded text-white cursor-pointer">Select from computer</label>
-                        <input
-                            id="inputFile"
-                            type="file"
-                            className="hidden"
-                            onChange={(e) => handleDrop(e, 'input')}
-                        />
-                    </div>
+                    {
+                        urlImage ?
+                        <Image className='absolute object-cover'
+                            src={urlImage}
+                            fill
+                            alt="Avatar"
+                        /> :    
+                        <>
+                            <div className="p-3 mt-10 flex justify-center">
+                                <Image src="/images/image_icon.png" alt="" width={100} height={100}></Image>
+                            </div>
+                            <div className="pb-3 flex justify-center text-xl">
+                                <span>Drag photos or videos here</span>
+                            </div>
+                            <div className="flex justify-center items-center w-100 mb-20 p-3">
+                                <label htmlFor="inputFile" className="bg-blue-600 hover:bg-blue-700 px-3 py-1 rounded text-white cursor-pointer">Select from computer</label>
+                                <input
+                                    id="inputFile"
+                                    type="file"
+                                    className="hidden"
+                                    onChange={(e) => handleDrop(e, 'input')} 
+                                />
+                            </div>
+                        </>
+                    }
                 </div>
             </div>
         </div>
